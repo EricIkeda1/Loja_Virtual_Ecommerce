82a212c79c48b4e76bded29e777776e580bc6c45
ikedayuji <ikedayuji.2002@gmail.com>
2024-07-29 21:36:21 -0300

    commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LojaComponent } from './loja/loja.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LojaComponent],  
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']        
 })
 export class AppComponent {
-  title = 'Loja_Virtual_Ecommerce';
+  title = 'areatechbr';
 }
